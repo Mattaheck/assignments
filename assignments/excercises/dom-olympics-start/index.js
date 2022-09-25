7044f4de5ad9961fbc6620faec12f16a18c9ad0c
Mattaheck <matthewaheck7u@gmail.com>
2022-09-25 13:07:42 -0500

    some updates

--- assignments/excercises/dom-olympics-start/index.js
+++ assignments/excercises/dom-olympics-start/index.js
@@ -16,6 +16,7 @@ subHeader.prepend(newSpan);
 newSpan.style.color = "yellow";
 newSpan.style.fontSize = "30px";
 
+
 var happyText = document.querySelectorAll("div.messages > div")
     // console.log(Array.from(happyText))
 
@@ -28,67 +29,63 @@ btn.addEventListener('click', function handleClick(){
     removeText.parentNode.removeChild(removeText);
 });
 
-
-var colorRight = document.getElementsByClassName("message right")
-    console.log(Array.from(colorRight));
-
-var colorLeft = document.getElementsByClassName("message left")
-    console.log(Array.from(colorLeft));
-
-// var colorSwitch = document.querySelectorAll("option")
-//     console.log(Array.from(colorSwitch))   
-    
-var value = selector.options[selector.selectedIndex].value;
-    console.log(value); 
-
-var dropColor = document.getElementById("selector");
-dropColor.addEventListener('click', function(){
-    if (value = "blue") {
-    for (var i = 0; i < colorRight.length; i++)
-        colorRight[i].style.backgroundColor = "blue";
-    } else if (value = "red") {
-        for (var i = 0; i < colorRight.length; i++)
-            colorRight[i].style.backgroundColor = "red";
-    // for (var i = 0; i < colorLeft.length; i++)
-    //     colorLeft[i].style.backgroundColor = "purple"
-    //  } else if (value = "red") {
-    // for (var i = 0; i < colorRight.length; i++)
-    //     colorRight[i].style.backgroundColor = "red";
-      
-    // for (var i = 0; i < colorLeft.length; i++)
-    //     colorLeft[i].style.backgroundColor = "black", colorLeft[i].style.color = "white"; 
-    // }
+function userText(event){
+    event.preventDefault()
+    var userMessage = document.getElementById("input").value;
+    if (i % 2 === 0){
+        document.querySelector(".messages").innerHTML += "<div class = 'message left'>" + userMessage + "</div>";
+    } else {document.querySelector(".messages").innerHTML += "<div class = 'message right'>" + userMessage + "</div>"};
+    themeChange()
+    document.getElementById("input").value = ""
     }
 
+var form = document.message
+form.addEventListener("submit", userText)
 
-}) 
+var submitButton = document.getElementById("send");
+
+// var colorRight = document.getElementsByClassName("message right")
+//     console.log(Array.from(colorRight));
+
+// var colorLeft = document.getElementsByClassName("message left")
+//     console.log(Array.from(colorLeft));
+
+// // var colorSwitch = document.querySelectorAll("option")
+// //     console.log(Array.from(colorSwitch))   
+    
+// var value = selector.options[selector.selectedIndex].value;
+//     console.log(value); 
+
+var messages = document.querySelector(".messages");
+var messageText = messages.querySelectorAll("div.message");
+var dropdown = document.getElementById("theme-drop-down");
+dropdown.addEventListener("change", themeChange)
+
+function themeChange (){
+    var messageText = messages.querySelectorAll("div.message")
+    console.log(messageText.length);
+
+    if (dropdown.value == "theme-one") {
+    for (var i = 0; i < messageText.length; i++){
+        if (i % 2 === 0){
+        messageText[i].style.backgroundColor = "blue"; 
+        } else if (i % 2 === 1)
+        
+        messageText[i].style.backgroundColor = "burlywood";
+        
+    }
+    } else if (dropdown.value == "theme-two") {
+        for (var i = 0; i < messageText.length; i++){
+        if (i % 2 === 0){
+            messageText[i].style.backgroundColor = "red";
+        } else if (i % 2 === 1)
+            messageText[i].style.backgroundColor = "black";
+            messageText[i].style.color = "white";
+           
     
 
+    }
+}
+}
 
 
-    
-
-
-
-
-
-
-   
-
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
-    
